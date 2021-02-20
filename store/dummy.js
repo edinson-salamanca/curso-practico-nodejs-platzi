66aa46fda17051cf0a1ea6ab0a3c8efc550f7ed8
Edinson_Salamanca <sennin7k@gmail.com>
2021-02-20 16:27:15 -0500

    separate the database code

---
+++ store/dummy.js
@@ -0,0 +1,32 @@
+const db = {
+  user: [
+    {
+      id: 1,
+      name: 'Carlos',
+    },
+  ],
+};
+
+function list(tabla) {
+  return db[tabla];
+}
+
+function get(tabla, id) {
+  let col = lis(tabla);
+  return col.filter((item) => item.id === id)[0] || null;
+}
+
+function upsert(tabla, data) {
+  db[collection].push(data);
+}
+
+function remove(tabla, id) {
+  return true;
+}
+
+module.exports = {
+  list,
+  get,
+  upsert,
+  remove,
+};

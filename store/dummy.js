106c949637c4f6b57a178e3c8086396a583c3d4c
Edinson_Salamanca <sennin7k@gmail.com>
2021-02-20 21:53:58 -0500

    route get for users

--- store/dummy.js
+++ store/dummy.js
@@ -1,26 +1,26 @@
 const db = {
   user: [
     {
-      id: 1,
+      id: '1',
       name: 'Carlos',
     },
   ],
 };
 
-function list(tabla) {
+async function list(tabla) {
   return db[tabla];
 }
 
-function get(tabla, id) {
-  let col = lis(tabla);
+async function get(tabla, id) {
+  let col = await list(tabla);
   return col.filter((item) => item.id === id)[0] || null;
 }
 
-function upsert(tabla, data) {
+async function upsert(tabla, data) {
   db[collection].push(data);
 }
 
-function remove(tabla, id) {
+async function remove(tabla, id) {
   return true;
 }
 

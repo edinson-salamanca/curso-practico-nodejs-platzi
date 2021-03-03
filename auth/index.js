f7a16ad4d03237fd717c7fb6d6cd7b1315c28f46
sennin <sennin@pop-os.localdomain>
2021-03-03 17:12:35 -0500

    permission management authentication

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,41 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
   return jwt.sign(data, 'secreto');
+}
+function verify(toke) {
+  return jwt.verify(toke, secret);
+}
+
+const check = {
+  own: function (req, owner) {
+    const decoded = decodeHeader(req);
+    console.log(decoded);
+  },
+};
+
+function getToken(auth) {
+  if (!auth) {
+    throw new Error('No viene token');
+  }
+  if (auth.indexOf('Bearer') === -1) {
+    throw new Error('Formato invalido');
+  }
+
+  let token = auth.replace('Bearer', '');
+
+  return token;
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || '';
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
 }
 
 module.exports = {
